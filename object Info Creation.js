032d5c3eb72ef2f860c0604729ba052bc75c44b2
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-02 17:59:51 +0300

    Add files via upload

---
+++ object Info Creation.js
@@ -0,0 +1,11 @@
+function infoCreation(name, last, age) {
+let personalInfo = {};
+personalInfo.firstName = name;
+personalInfo.lastName = last;
+personalInfo.age = age;
+
+return personalInfo;
+}
+infoCreation("George", 
+"Smith",
+"18");
